98c7cd54c3aed1db46f99d231056712c8472c961
Шевцов Виктор <Shevczov0751@mail.ru>
2026-06-28 13:11:46 +0700

    чуть красивее

--- app/src/main/java/com/example/myapplication/style/InstrumentStyle.kt
+++ app/src/main/java/com/example/myapplication/style/InstrumentStyle.kt
@@ -4,11 +4,40 @@ import android.graphics.Color
 
 object InstrumentStyle {
 
-    val caseColor = Color.rgb(45,45,45)
-    val dialColor = Color.BLACK
+    /* Цвет корпуса прибора */
+    val caseColor = Color.rgb(45, 45, 45)
+
+    /* Основной фон шкалы */
+    val dialColor = Color.rgb(15, 15, 15)
+
+    /* Цвет делений */
+    val scaleColor = Color.rgb(200, 200, 200)
+
+    /* Цвет текста */
     val textColor = Color.WHITE
+
+    /* Основная стрелка */
     val needleColor = Color.WHITE
-    val skyColor = Color.rgb(70,140,255)
-    val groundColor = Color.rgb(160,90,40)
+
+    /* Вспомогательная стрелка */
+    val secondaryNeedleColor = Color.rgb(255, 80, 80)
+
+    /* Цвет неба (авиагоризонт) */
+    val skyColor = Color.rgb(70, 140, 255)
+
+    /* Цвет земли */
+    val groundColor = Color.rgb(160, 90, 40)
+
+    /* Цвет линии горизонта */
+    val horizonLineColor = Color.WHITE
+
+    /* Цвет рамки прибора */
+    val bezelColor = Color.rgb(90, 90, 90)
+
+    /* Внутренняя тень */
+    val shadowColor = Color.argb(120, 0, 0, 0)
+
+    /* Янтарная авиационная подсветка */
+    val amberTextColor = Color.rgb(255, 200, 80)
 
 }
